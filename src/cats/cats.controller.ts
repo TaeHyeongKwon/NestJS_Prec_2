f62c8e56ac2c9f2b7a465c89b332006e83aacd9e
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-12 02:39:16 +0900

    feat/고양이 DB 연결 API 연습

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './entity/cats.entity';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private catsService: CatsService) {}
+
+  @Get()
+  findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Cat> {
+    return this.catsService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() cat: Cat) {
+    this.catsService.create(cat);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() cat: Cat) {
+    this.catsService.update(id, cat);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    return this.catsService.remove(id);
+  }
+}
