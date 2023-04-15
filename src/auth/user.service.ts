e47a89055b246f7e1ebfa96a98d8d81061529a18
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-15 23:57:37 +0900

    fix/비밀번호 해싱처리 추가로 인한 회원가입API수정

--- src/auth/user.service.ts
+++ src/auth/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { FindOneOptions, Repository } from 'typeorm';
 import { UserDTO } from './dto/user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -17,6 +18,12 @@ export class UserService {
   }
 
   async save(userDTO: UserDTO): Promise<UserDTO | undefined> {
+    await this.transformPassword(userDTO);
     return await this.userRepository.save(userDTO);
+  }
+
+  async transformPassword(user: UserDTO): Promise<void> {
+    user.password = await bcrypt.hash(user.password, 10);
+    return Promise.resolve();
   }
 }
