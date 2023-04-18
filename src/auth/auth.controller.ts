74c25a17132348b4f8aeb756717f34885b009ef1
TaeHyeongKwon <1004kth1313@naver.com>
2023-04-18 16:46:57 +0900

    fix/login에 토큰 추가 및 확인을 위한 API추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,40 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDTO } from './dto/user.dto';
+import { Request, Response } from 'express';
+import { AuthGuard } from './security/auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/register')
-  async registerAccount(
-    @Req() req: Request,
-    @Body() newUser: UserDTO,
-  ): Promise<any> {
+  async registerAccount(@Body() newUser: UserDTO): Promise<any> {
     return await this.authService.registerUser(newUser);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('/login')
-  async login(@Body() userDTO: UserDTO): Promise<any> {
-    return await this.authService.validateUser(userDTO);
+  async login(@Body() userDTO: UserDTO, @Res() res: Response): Promise<any> {
+    const jwt = await this.authService.validateUser(userDTO);
+    res.setHeader('Authorizetion', 'Bearer ' + jwt.accessToken);
+    return res.json(jwt);
+  }
+
+  @Get('/authenticate')
+  @UseGuards(AuthGuard)
+  isAuthenticated(@Req() req: Request): any {
+    const user: any = req.user;
+    return user;
   }
 }
